7afd3411a5ea96af6d2a76f4ba36b911ab71fadc
Ahmed <ahmedothman.rc@gmail.com>
2021-12-08 13:38:16 +0400

    Qr Save

--- lib/Screens/QR-Code/componants/body.dart
+++ lib/Screens/QR-Code/componants/body.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:tsareeh/Screens/QR-Code/componants/qr_header.dart';
 import 'package:tsareeh/components/rounded_button.dart';
@@ -5,6 +9,8 @@ import 'package:qr_flutter/qr_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tsareeh/Screens/Signup/Models/user_modle.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../../../constants.dart';
 
@@ -13,12 +19,21 @@ import '../../../constants.dart';
 
 
 class Body extends StatefulWidget {
+
+
   _Body createState() => _Body();
 
 }
 class _Body extends State<Body> {
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
+
+
+
+
+
+
+
 
   @override
   void initState(){
@@ -32,11 +47,17 @@ class _Body extends State<Body> {
 
       });
     });
+
+
   }
 
 
   @override
   Widget build(BuildContext context) {
+
+
+
+
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
@@ -45,7 +66,7 @@ class _Body extends State<Body> {
             SizedBox(height:50),
             QRHeader(),
             SizedBox(height:80),
-             QrImage(
+            QrImage(
              data: '${loggedInUser.qrCode}',
              size: 300,
            ),
@@ -71,7 +92,7 @@ class _Body extends State<Body> {
             RoundedButton(
               text: "Save",
               press: () {
-
+                createQrPicture(loggedInUser.qrCode.toString());
               },
             ),
 
@@ -85,6 +106,41 @@ class _Body extends State<Body> {
           ],
         ),
       ),
+    );
+  }
+
+  Future<String> createQrPicture(String qr) async {
+
+    final qrValidationResult = QrValidator.validate(
+      data: '${loggedInUser.qrCode}',
+      version: QrVersions.auto,
+      errorCorrectionLevel: QrErrorCorrectLevel.L,
+    );
+    qrValidationResult.status == QrValidationStatus.valid;
+    qrValidationResult.error;
+    QrCode? qrCode = qrValidationResult.qrCode;
+
+    final painter = QrPainter.withQr(
+      qr: qrCode!,
+      color: const Color(0xFF000000),
+      gapless: true,
+      embeddedImageStyle: null,
+      embeddedImage: null,
+    );
+
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+    final ts = DateTime.now().millisecondsSinceEpoch.toString();
+    String path = '$tempPath/$ts.png';
+    final picData = await painter.toImageData(2048, format: ImageByteFormat.png);
+    await writeToFile(picData!, path);
+    return path;
+  }
+
+  Future<void> writeToFile(ByteData data, String path) async {
+    final buffer = data.buffer;
+    await File(path).writeAsBytes(
+        buffer.asUint8List(data.offsetInBytes, data.lengthInBytes)
     );
   }
 }
